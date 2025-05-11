7114bed70cecdf6eb007a8e9169f6395638073ca
Lipi7dev <lipi7mdrs@gmail.com>
2025-05-11 18:53:33 +0000

    Entrada e saida da carta  1 e 2

--- CartasSuperTrunfo.c
+++ CartasSuperTrunfo.c
@@ -42,22 +42,39 @@ int main () {
     printf("pib: %f \n",pib);
     printf("Pontos turisticos: %d \n",pontosturisticos);
 
+    printf("carta 2\n");
+
+    printf("Digite o nome do estado\n");
+    scanf("%s",estado);
+
+    printf("Digite o código da carta\n");
+    scanf("%s",codigo);
+
+    printf("Digite o nome da cidade\n");
+    scanf("%s",cidade);
+
+    printf("Qual o tamanho da populacão\n");
+    scanf("%d",&populacao);
+
+    printf("Digite o tamanho da cidade em km\n");
+    scanf("%f",&area);
+
+    printf("Digite o pib da cidade\n");
+    scanf("%f",&pib);
+
+    printf("Digite a quantidade de pontos turisticos\n");
+    scanf("%d",&pontosturisticos);
+
+    printf("Carta 2\n");
     
+    printf("Estado: %s \n",estado);
+    printf("Código da carta: %s \n",codigo);
+    printf("Cidade: %s \n",cidade);
+    printf("populacão: %d \n",populacao);
+    printf("Área: %f \n",area);
+    printf("pib: %f \n",pib);
+    printf("Pontos turisticos: %d \n",pontosturisticos);
 
-
-
-  
-
-
-
-   
-
-
-
-
-
-    
-
-
+    return 0;
 
 }
